11601f075d27d7d661443c6139c02c6df4b40319
PFLYIN <pedrodiegomeira@gmail.com>
2025-04-15 17:25:46 -0300

    tamos indo

--- JS/menu.js
+++ JS/menu.js
@@ -1,10 +1,9 @@
-<script>
-    document.addEventListener("DOMContentLoaded", function () {
-        const toggle = document.querySelector(".menu-toggle");
-        const nav = document.querySelector(".nav-desktop");
+document.addEventListener("DOMContentLoaded", function () {
+    const toggle = document.querySelector(".menu-toggle");
+    const nav = document.querySelector(".nav-desktop");
 
-        toggle.addEventListener("click", function () {
-            nav.classList.toggle("active");
-        });
+    toggle.addEventListener("click", function () {
+        nav.classList.toggle("active");
     });
-</script>
+});
+
